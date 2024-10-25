6265bdd6045c5d684bd06df288f279989e197a22
Carla Ribeiro <carlaribeiro.ads@gmail.com>
2024-10-25 14:46:44 -0300

    feat: dados dinâmicos via array.
    Inclusão dos dados dinamicamente.

---
+++ js/dados.js
@@ -0,0 +1,444 @@
+const url = "./imagens/miniaturas/";
+
+
+
+const dadosBeleza = [
+    {
+        "id": 1,
+        "logo": url + "4.jpg",
+        "loja": "Aninha - Cosméticos e acessórios",
+        "contato": "(00)00000-0000"
+    },
+    {
+        "id": 2,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+    {
+        "id": 3,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+    {
+        "id": 4,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+    {
+        "id": 5,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+    {
+        "id": 6,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    }
+]
+
+const dadosVestuario = [
+    {
+        "id": 1,
+        "logo": url + "4.jpg",
+        "loja": "Aninha - Cosméticos e acessórios",
+        "contato": "(00)00000-0000"
+    },
+    {
+        "id": 2,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+    {
+        "id": 3,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+    {
+        "id": 4,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+    {
+        "id": 5,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+    {
+        "id": 6,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+]
+
+const dadosCulinariaRefeicao = [
+    {
+        "id": 1,
+        "logo": url + "17.png",
+        "loja": "Delícias da Casa",
+        "contato": "(21)98033-8459"
+    },
+    {
+        "id": 2,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+    {
+        "id": 3,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+    {
+        "id": 4,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+    {
+        "id": 5,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+    {
+        "id": 6,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+]
+
+const dadosCulinariaLanchesSalgados = [
+    {
+        "id": 1,
+        "logo": url + "11.jpg",
+        "loja": "Milena Santana - Doces e Salgados",
+        "contato": "(21) 99793-5764"
+    },
+    {
+        "id": 2,
+        "logo": url + "22.png",
+        "loja": "Lanches da Milla",
+        "contato": "(21) 98135-4237"
+    },
+    {
+        "id": 3,
+        "logo": url + "29.jpg",
+        "loja": "Top Lanches",
+        "contato": "(21) 99042-8931"
+    },
+    {
+        "id": 4,
+        "logo": url + "27.jpg",
+        "loja": "Salgadinhos da Dona Graça",
+        "contato": "(21) 96498-7676"
+    },
+    {
+        "id": 5,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+    {
+        "id": 6,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+]
+
+const dadosCulinariaDoce = [
+    {
+        "id": 1,
+        "logo": url + "1.jpg",
+        "loja": "Brigs Meli",
+        "contato": "(21) 97893-6950"
+    },
+    {
+        "id": 2,
+        "logo": url + "8.jpg",
+        "loja": "Doce Eschellin Confeitaria",
+        "contato": "(21) 99487-0609"
+    },
+
+    {
+        "id": 3,
+        "logo": url + "11.jpg",
+        "loja": "Milena Santana - Doces e Salgados",
+        "contato": "(21) 99793-5764"
+    },
+    {
+        "id": 4,
+        "logo": url + "24.jpg",
+        "loja": "Açai da Lu",
+        "contato": "(21) 98473-3967"
+    },
+    {
+        "id": 5,
+        "logo": url + "26.jpg",
+        "loja": "G & T Delícias",
+        "contato": "(21) 98772-9136"
+    },
+    {
+        "id": 6,
+        "logo": url + "31.jpg",
+        "loja": "Janaina - Sacolé Gourmet",
+        "contato": "(21) 99887-0695"
+    }
+
+]
+
+
+
+const dadosFesta = [
+    {
+        "id": 1,
+        "logo": url + "14.jpg",
+        "loja": "Ki Sabor - Maça do Amor",
+        "contato": "(21) 97057-7326"
+    },
+    {
+        "id": 2,
+        "logo": url + "15.jpg",
+        "loja": "Cake Amadora",
+        "contato": "(21) 98758-0021"
+    },
+    {
+        "id": 3,
+        "logo": url + "11.jpg",
+        "loja": "Milena Santana - Doces e Salgados",
+        "contato": "(21) 99793-5764"
+    },
+    {
+        "id": 4,
+        "logo": url + "9.jpg",
+        "loja": "Brenda Ramilo Confeitaria Gourmet",
+        "contato": "(21) 97150-5220"
+    },
+    {
+        "id": 5,
+        "logo": url + "8.jpg",
+        "loja": "Doce Eschellin Confeitaria",
+        "contato": "(21) 99487-0609"
+    },
+    {
+        "id": 6,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+]
+
+const dadosEducacao = [
+    {
+    "id": 1,
+    "logo": url + "1000.png",
+    "loja": "",
+    "contato": ""
+},
+{
+    "id": 2,
+    "logo": url + "1000.png",
+    "loja": "",
+    "contato": ""
+},
+{
+    "id": 3,
+    "logo": url + "1000.png",
+    "loja": "",
+    "contato": ""
+},
+{
+    "id": 4,
+    "logo": url + "1000.png",
+    "loja": "",
+    "contato": ""
+},
+{
+    "id": 5,
+    "logo": url + "1000.png",
+    "loja": "",
+    "contato": ""
+},
+{
+    "id": 6,
+    "logo": url + "1000.png",
+    "loja": "",
+    "contato": ""
+}
+]
+
+const dadosArtesanato = [
+    {
+        "id": 1,
+        "logo": url + "13.jpg",
+        "loja": "Leeh - Crochet",
+        "contato": "(21) 00000-0000"
+    },
+    {
+        "id": 2,
+        "logo": url + "5.jpg",
+        "loja": "Cida Gomes - Arte com agulhas",
+        "contato": "(21) 99073-4654"
+    },
+    {
+        "id": 3,
+        "logo": url + "7.jpg",
+        "loja": "Cintia Arts",
+        "contato": "(21) 99221-6057"
+    },
+    {
+        "id": 4,
+        "logo": url + "23.png",
+        "loja": "Gallileu Papelaria",
+        "contato": "(21) 98547-0433"
+    }
+
+]
+
+const dadosDigitalTech = [
+    {
+        "id": 1,
+        "logo": url + "3.jpg",
+        "loja": "Carla Ribeiro - Desenvolvimento Web",
+        "contato": "(21) 98349-1846"
+    },
+    {
+        "id": 2,
+        "logo": url + "23.png",
+        "loja": "Gallileu Papelaria",
+        "contato": "(21) 98547-0433"
+    },
+    {
+        "id": 3,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+    {
+        "id": 4,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+    {
+        "id": 5,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    },
+    {
+        "id": 6,
+        "logo": url + "1000.png",
+        "loja": "",
+        "contato": ""
+    }
+]
+
+
+for (cardBeleza of dadosBeleza) {
+    let sessaoBeleza = document.getElementById('segmento-beleza');
+    let cardsBeleza = `<div class="card">
+    <img class="logo" src="${cardBeleza.logo}"/>
+    <p class="infoCard">${cardBeleza.loja}</p>
+    <p class="infoCard">${cardBeleza.contato}</p>
+    </div>`
+    sessaoBeleza.innerHTML += cardsBeleza;
+
+}
+
+for (cardCulinaria of dadosCulinariaRefeicao) {
+    let sessaoCulinaria = document.getElementById('segmento-comida-caseira');
+    let cardsCulinaria = `<div class="card">
+    <img class="logo" src="${cardCulinaria.logo}"/>
+    <p class="infoCard">${cardCulinaria.loja}</p>
+    <p class="infoCard">${cardCulinaria.contato} </p>
+    </div>`
+    sessaoCulinaria.innerHTML += cardsCulinaria;
+
+}
+
+for (cardLanches of dadosCulinariaLanchesSalgados) {
+    let sessaoLanches = document.getElementById('segmento-lanches-salgados');
+    let cardsLanches = `<div class="card">
+<img class="logo" src="${cardLanches.logo}"/>
+<p class="infoCard">${cardLanches.loja}</p>
+<p class="infoCard">${cardLanches.contato}</p>
+</div>`
+
+    sessaoLanches.innerHTML += cardsLanches;
+}
+
+
+for (cardDoces of dadosCulinariaDoce) {
+    let sessaoDoces = document.getElementById('segmento-doces-confeitaria');
+    let cardsDoces = `<div class="card">
+    <img class="logo" src="${cardDoces.logo}"/>
+    <p class="infoCard">${cardDoces.loja}</p>
+    <p class="infoCard">${cardDoces.contato}</p>
+    </div>`
+    sessaoDoces.innerHTML += cardsDoces;
+
+}
+
+for (cardVestuario of dadosVestuario) {
+    let sessaoVestuario = document.getElementById('segmento-moda-vestuario');
+    let cardsVestuario = `<div class="card">
+    <img class="logo" src="${cardVestuario.logo}"/>
+    <p class="infoCard">${cardVestuario.loja}</p>
+    <p class="infoCard">${cardVestuario.contato}</p>
+    </div>`
+    sessaoVestuario.innerHTML += cardsVestuario;
+}
+
+for (cardFestaDecor of dadosFesta) {
+    let sessaoFestaDecor = document.getElementById('segmento-festa-decoracao');
+    let cardsFestaDecor = `<div class="card">
+    <img class="logo" src="${cardFestaDecor.logo}"/>
+    <p class="infoCard">${cardFestaDecor.loja}</p>
+    <p class="infoCard">${cardFestaDecor.contato}</p>
+    </div>`
+    sessaoFestaDecor.innerHTML += cardsFestaDecor;
+}
+
+for (cardEnsino of dadosEducacao) {
+    let sessaoEnsino = document.getElementById('segmento-educacao-ensino');
+    let cardsEnsino = `<div class="card">
+    <img class="logo" src="${cardEnsino.logo}"/>
+    <p class="infoCard">${cardEnsino.loja}</p>
+    <p class="infoCard">${cardEnsino.contato}</p>
+    </div>`
+    sessaoEnsino.innerHTML += cardsEnsino;
+}
+
+for (cardArtesanato of dadosArtesanato) {
+    let sessaoArtesanato = document.getElementById('segmento-artesanato-papelaria');
+    let cardsArtesanato = `<div class="card">
+    <img class="logo" src="${cardArtesanato.logo}"/>
+    <p class="infoCard">${cardArtesanato.loja}</p>
+    <p class="infoCard">${cardArtesanato.contato}</p>
+    </div>`
+    sessaoArtesanato.innerHTML += cardsArtesanato;
+}
+
+for (cardDigitalTech of dadosDigitalTech) {
+    let sessaoDigitalTech = document.getElementById('segmento-servicos-digitais');
+    let cardsDigitalTech = `<div class="card">
+    <img class="logo" src="${cardDigitalTech.logo}"/>
+    <p class="infoCard">${cardDigitalTech.loja}</p>
+    <p class="infoCard">${cardDigitalTech.contato}</p>
+    </div>`
+    sessaoDigitalTech.innerHTML += cardsDigitalTech;
+
+}
